743315bff5cf547a9a4dc006246a022bfc593c80
choquitofrito <zyriab@gmail.com>
2020-10-06 09:06:38 +0200

    added exemples

---
+++ Initiation à la programmation en PHP/Formulaires/Solutions/Ex01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="./Ex01Traitement.php" method="POST">
+        Prénom<input type="text" name="prenom">
+        Nom<input type="text" name="nom"> 
+        Age<input type="number" name="age"> 
+        <input type="submit">
+    </form>
+</body>
+</html>
