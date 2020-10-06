c72898691897c056846c8bc4c318e58faec3ee31
choquitofrito <zyriab@gmail.com>
2020-10-06 16:13:35 +0200

    insert ok

--- Initiation à la programmation en PHP/BasesDonnees/Exemples/Exemple1InsertTraitement.php
+++ Initiation à la programmation en PHP/BasesDonnees/Exemples/Exemple1InsertTraitement.php
@@ -1,25 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
     include "./config/db.php";
     // créer une connexion à la BD
-    $db= new PDO (DBDRIVER.': host='.DBHOST.';port='.DBPORT.';dbname='.DBNAME.
-                ';charset='.DBCHARSET, DBUSER, DBPASS); 
-    
-    
-    var_dump ($_POST);
+    $db = new PDO(DBDRIVER . ': host=' . DBHOST . ';port=' . DBPORT . ';dbname=' . DBNAME .
+        ';charset=' . DBCHARSET, DBUSER, DBPASS);
 
-    $sql = "INSERT INTO ('id','code','villeDepart','villeDestination') ".
-            "VALUES ( )";
+    // prise des valeurs du formulaire
+    $code = $_POST['code']; // manque filter_vars
+    $villeDepart = $_POST['villeDepart'];
+    $villeDestination = $_POST['villeDestination'];
 
-    
+    // création de la requête et spécification des paramètres
+    $sql = "INSERT INTO trains (id,code,villeDepart,villeDestination) " .
+        "VALUES (null, :code,:villeDepart,:villeDestination)";
+
+    // préparation de la requête
+    $stmt = $db->prepare($sql);
+
+    // donner des valeurs aux paramètres
+    $stmt->bindParam (":code",$code);
+    $stmt->bindParam (":villeDepart",$villeDepart);
+    $stmt->bindParam (":villeDestination",$villeDestination);
+
+    // lancer la requête
+    $stmt->execute();
+    var_dump ($stmt->errorInfo());
+    var_dump ($db->errorInfo());
+
     ?>
 </body>
-</html>
 
+</html>
