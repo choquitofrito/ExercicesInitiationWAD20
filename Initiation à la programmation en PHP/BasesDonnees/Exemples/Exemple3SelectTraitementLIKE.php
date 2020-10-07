b5259da60e05cff41266281f3ab618c766968e5b
choquitofrito <zyriab@gmail.com>
2020-10-07 10:49:38 +0200

    après exemple Like

--- Initiation à la programmation en PHP/BasesDonnees/Exemples/Exemple3SelectTraitementLIKE.php
+++ Initiation à la programmation en PHP/BasesDonnees/Exemples/Exemple3SelectTraitementLIKE.php
@@ -13,11 +13,12 @@
                 ';charset='.DBCHARSET, DBUSER, DBPASS); 
 
     // prise de données d'un formulaire
-    $villeDepart = $_POST['villeDepart']; // filter_vars
+    $villeDepart = "%".$_POST['villeDepart']."%"; // filter_vars
     $villeDepart = strtoupper($villeDepart);
     
-    $sql = "SELECT * FROM trains WHERE villeDepart=:villeDepart";
+    $sql = "SELECT * FROM trains WHERE villeDepart LIKE :villeDepart";
 
+    
     var_dump ($sql);
 
     $stmt = $db->prepare($sql); // envoyer la requête au serveur
@@ -28,6 +29,25 @@
     $arrayResultat = $stmt->fetchAll(PDO::FETCH_ASSOC); // créer un array qui contient des arrays assoc
 
     var_dump ($arrayResultat);
+
+
+    // attention à ce qu'on passe à bindParam (référence)
+    function changeNomFake ($unNom){
+        $unNom = "Lucy";
+        echo $unNom;
+    }
+    $n = "Sophie";
+    changeNomFake ($n); 
+    // echo $n; // Sophie
+    changeNomFake ("Luna");
+
+    function changeNom (&$unNom){
+        $unNom = "Lucy";
+    }
+    $n = "Sophie";
+    changeNom ($n);
+    // changeNom ("Luna"); // erreur car le param est par référence
+
 
 
     ?>
--- Initiation à la programmation en PHP/BasesDonnees/Exemples/sql/exemplesSQL.sql
+++ Initiation à la programmation en PHP/BasesDonnees/Exemples/sql/exemplesSQL.sql
@@ -15,6 +15,26 @@ SELECT id,code,UPPER(villeDepart),UPPER(villeDestination) FROM trains
 SELECT id, code, villeDepart, villeDestination FROM trains WHERE LEFT(villeDepart,3) = 'Bru'
 -- ex en PHP : $sql = "SELECT * FROM trains WHERE UPPER(villeDepart)=:villeDepart";
 
+-- LIKE: Selection avec un fragment de la donnée
+-- ex: tous les trains dont la ville de départ commence par 'Bru'
+SELECT * FROM trains WHERE villeDepart LIKE 'Bru%' 
+-- ex: tous les trains dont la ville de départ finit par 's'
+SELECT * FROM trains WHERE villeDepart LIKE '%es' 
+-- ex: tous les trains dont la ville contient un certain texte
+SELECT * FROM trains WHERE villeDepart LIKE '%xe%'
+
+SELECT * FROM trains WHERE villeDepart LIKE '%rs%' OR villeDestination LIKE '%en%'
+
+
+
+
+
+
+
+
+
+
+
 
 
 
