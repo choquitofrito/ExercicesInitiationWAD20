4c086f230b9f311d7ab69a20b1965ad273e39f8b
choquitofrito <bender@localhost.localdomain>
2020-10-16 11:46:38 +0200

    apres que Sophie a detruit ma partition

--- POO/Fondements/Exemples/06-Interfaces/accueil.php
+++ POO/Fondements/Exemples/06-Interfaces/accueil.php
@@ -1,19 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
     <?php
-    $somme = function ($a, $b) {
-        echo "somme: " . ($a + $b);
-    };
-    $somme(10, 20);
+    
+
     ?>
 </body>
-
 </html>
