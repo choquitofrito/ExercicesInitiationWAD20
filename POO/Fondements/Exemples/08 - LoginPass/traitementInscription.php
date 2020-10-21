f590460705e04fcaa46d5b9b12905334d8c8a2ce
choquitofrito <zyriab@gmail.com>
2020-10-21 09:53:51 +0200

    start login pass

---
+++ POO/Fondements/Exemples/08 - LoginPass/traitementInscription.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        var_dump ($_POST);
+        // 1. Connecter à la BD
+
+        // 2. Vérifier passwords pareils
+
+        // 3. Hasher le password
+
+        // 4. Créer une requête INSERT
+
+
+        // 5. Lancer la requête
+
+    ?>
+</body>
+</html>
