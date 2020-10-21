88a8552537b51b6a7f3f483a39e91fcd763c2864
choquitofrito <zyriab@gmail.com>
2020-10-21 10:29:44 +0200

    start login pass

--- POO/Fondements/Exemples/08 - LoginPass/traitementInscription.php
+++ POO/Fondements/Exemples/08 - LoginPass/traitementInscription.php
@@ -1,34 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        var_dump ($_POST);
-        // 1. Connecter à la BD
-        include "./config/db.php";
-        // créer une connexion à la BD
-        try {
-            $db = new PDO(DBDRIVER . ': host=' . DBHOST . ';port=' . DBPORT . ';dbname=' . DBNAME .
-                ';charset=' . DBCHARSET, DBUSER, DBPASS);
-        } catch (Exception $e) {
-            echo "Erreur";
-            die();
-        }
+    // var_dump($_POST);
     
+    // 1. Connecter à la BD
 
-        // 2. Vérifier passwords pareils (du form, pas avec la BD)
+    include "./config/db.php";
+    // créer une connexion à la BD
+    try {
+        $db = new PDO(DBDRIVER . ': host=' . DBHOST . ';port=' . DBPORT . ';dbname=' . DBNAME .
+            ';charset=' . DBCHARSET, DBUSER, DBPASS);
+    } catch (Exception $e) {
+        echo "Erreur";
+        die();
+    }
 
-        // 3. Hasher le password
+    // 2. Vérifier passwords pareils (du form, pas avec la BD)
+    $mot_pass = $_POST['mot_pass'];
+    $mot_pass_copy = $_POST['mot_pass_copy'];
+    if ($mot_pass != $mot_pass_copy){
+        echo "Les passwords ne coincident pas";
+        die();
+        // header ("location: ....")
+    } 
+    // 3. Hasher le password
+    $mot_pass_hash = password_hash($mot_pass, PASSWORD_DEFAULT, ['cost'=>12]);
+    // 4. Créer une requête INSERT
+    var_dump ($mot_pass_hash);
 
-        // 4. Créer une requête INSERT
-
-
-        // 5. Lancer la requête
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $adresse = $_POST['adresse'];
+    $email = $_POST['email'];
+    
+    // 5. Lancer la requête
+    $sql = "INSERT INTO client (id, nom, prenom, adresse, email, mot_pass) "
+        ."VALUES (null, :nom, :prenom, :adresse, :email, :mot_pass)";
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(":nom", $nom);
+    $stmt->bindValue(":prenom", $prenom);
+    $stmt->bindValue(":adresse", $adresse);
+    $stmt->bindValue(":email", $email);
+    $stmt->bindValue(":mot_pass", $mot_pass);
+    $stmt->execute();
+    var_dump ($stmt->errorInfo());
+    
 
     ?>
 </body>
+
 </html>
