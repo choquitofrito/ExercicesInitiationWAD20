dec8c5e962acfd58c35c2937bbb29f213c709727
choquitofrito <zyriab@gmail.com>
2020-10-26 16:40:09 +0100

    session login-pass end

---
+++ POO/Fondements/Exemples/Annexe 01 - Header problem/11-HeaderProblem.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document
+    Document
+    Document
+    Document
+    </title>
+</head>
+<body>
+    <?php 
+    echo "Loulou"; 
+    // ne pas générer du HTML, ni pur ni dans PHP, avant de faire un header
+    header ("location: ./autrePage.php");
+
+    ?>
+</body>
+</html>
